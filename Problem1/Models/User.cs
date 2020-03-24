a3ddee10e4b486a9c5d64135d7afcdf52c0e59ac
pratik-jain <35480147+pratik-jain@users.noreply.github.com>
2020-03-24 18:07:43 +0530

    Add files via upload

---
+++ Problem1/Models/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problem1.Models   
+{
+    class User
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string MobileNumebr { get; set; }
+        public string Email { get; set; }
+        public string DOB { get; set; }
+        public string Gender { get; set; }
+        public string Address { get; set; }
+        public string Password { get; set; }
+    }
+}
