598a28397e2130489807daaa65e0d4234d6f2fb3
pratik-jain <35480147+pratik-jain@users.noreply.github.com>
2020-04-03 09:02:43 +0530

    Add files via upload

---
+++ PartitionSql.sql
@@ -0,0 +1,36 @@
+CREATE PARTITION FUNCTION OrderPartition (datetime)
+AS RANGE RIGHT FOR VALUES ('2011-10-13 00:00', '2012-10-13 00:00', '2013-10-13 00:00');
+GO
+
+CREATE PARTITION SCHEME OrderByYear AS PARTITION OrderPartition TO (Order1,Order1,Order1,Order1);
+GO
+
+CREATE TABLE [Sales].[SalesOrderHeader_Partitioned](
+	[SalesOrderID] [int] IDENTITY(1,1) NOT FOR REPLICATION NOT NULL,
+	[RevisionNumber] [tinyint] NOT NULL,
+	[OrderDate] [datetime] NOT NULL,
+	[DueDate] [datetime] NOT NULL,
+	[ShipDate] [datetime] NULL,
+	[Status] [tinyint] NOT NULL,
+	[OnlineOrderFlag] [dbo].[Flag] NOT NULL,
+	[SalesOrderNumber]  AS (isnull(N'SO'+CONVERT([nvarchar](23),[SalesOrderID]),N'*** ERROR ***')),
+	[PurchaseOrderNumber] [dbo].[OrderNumber] NULL,
+	[AccountNumber] [dbo].[AccountNumber] NULL,
+	[CustomerID] [int] NOT NULL,
+	[SalesPersonID] [int] NULL,
+	[TerritoryID] [int] NULL,
+	[BillToAddressID] [int] NOT NULL,
+	[ShipToAddressID] [int] NOT NULL,
+	[ShipMethodID] [int] NOT NULL,
+	[CreditCardID] [int] NULL,
+	[CreditCardApprovalCode] [varchar](15) NULL,
+	[CurrencyRateID] [int] NULL,
+	[SubTotal] [money] NOT NULL,
+	[TaxAmt] [money] NOT NULL,
+	[Freight] [money] NOT NULL,
+	[TotalDue]  AS (isnull(([SubTotal]+[TaxAmt])+[Freight],(0))),
+	[Comment] [nvarchar](128) NULL,
+	[rowguid] [uniqueidentifier] ROWGUIDCOL  NOT NULL,
+	[ModifiedDate] [datetime] NOT NULL, 
+) ON OrderByYear(OrderDate)
+GO
