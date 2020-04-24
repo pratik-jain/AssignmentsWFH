52c3a5592ea3d51febb75f73042f3d13961f7f2f
pratik-jain <35480147+pratik-jain@users.noreply.github.com>
2020-04-24 10:03:43 +0530

    Add files via upload

---
+++ Module14LabEXercize.sql
@@ -0,0 +1,27 @@
+DECLARE @xmlSchema nvarchar(max)  
+Set @xmlSchema  = N' copy the schema collection here'  
+CREATE XML SCHEMA COLLECTION MyCollection AS @xmlSchema   
+
+Select * FROM Students FOR XML AUTO
+Select * FROM Students FOR XML RAW
+Select * FROM Students FOR XML PATH
+
+SELECT 1 AS TAG,NULL AS PARENT,
+	s.StudentId AS[Student!1!StudentId!ELEMENT],
+	s.StudentName AS[Student!1!StudentName!ELEMENT], 
+	s.Age AS[Student!1!Age!ELEMENT] 
+	from Students s FOR XML EXPLICIT
+
+
+Create PROC production.GetAvailableModelsAsXML
+	AS BEGIN
+	SELECT * FROM Production.Products ORDER BY SellStartDate,ProductName ASC FOR XML RAW,ROOT('AvaiableModels')
+	END
+
+alter PROC dbo.UpdateSalesTerritoriesByXML @SalesPersonMods as xml
+AS BEGIN
+	Declare @terrId int 
+	set @terrId = ( SELECT Results.Student.value('StudentId[1]','int') AS StudentId
+		FROM @SalesPersonMods.nodes('Student') Results(Student))
+	update Sales.SalesPerson SET SalesTerritoryId = @terrId
+END
